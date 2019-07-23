e712ff9751b1974e93427914abe4a8f85adcaa8a
Johann Montfort <jmonfu@gmail.com>
2019-07-23 16:42:07 +0200

    adding the useEffect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import uuid from 'uuid/v1';
 import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+    const [age, setAge] = useState(20);
+
     const [songs, setSongs] = useState([
         {title: 'almost home', id: 1},
         {title: 'memory gospel', id: 2},
@@ -13,6 +15,10 @@ const SongList = () => {
         setSongs([...songs, {title, id:uuid()}])
     }
 
+    useEffect(() => {
+        console.log('useEffect Hook ran');
+    }, [songs])
+
     return (  
         <div className="song-list">
             <ul>
@@ -21,6 +27,7 @@ const SongList = () => {
                 })}
             </ul>
             <NewSongForm addSong={addSong} />
+            <button onClick={() => setAge(age + 1)}>Add 1 To Age: {age}</button>
         </div>
     );
 }
